1476a68435b5ca3d49fc01afd60ee5bc0abc7aae
niuyafei <619994647@qq.com>
2017-11-07 01:20:34 +0800

    no message

--- frontend/controllers/SiteController.php
+++ frontend/controllers/SiteController.php
@@ -47,7 +47,7 @@ class SiteController extends Controller
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
-                    'logout' => ['post'],
+                    'logout' => ['get'],
                 ],
             ],
         ];
