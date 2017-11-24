8f272a4f3fc2badd70ca8cd4330a5b5756cef328
niuyafei <619994647@qq.com>
2017-11-24 17:25:04 +0800

    no message

--- backend/views/expert/index.php
+++ backend/views/expert/index.php
@@ -87,7 +87,7 @@ $this->title = '专家管理';
         <div class="modal-content" style="width:500px;">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-                <h5 class="modal-title color-blue">账号添加</h5>
+                <h5 class="modal-title color-blue">添加专家</h5>
             </div>
             <div class="modal-body">
                 <!--                <form class="form-horizontal">-->
